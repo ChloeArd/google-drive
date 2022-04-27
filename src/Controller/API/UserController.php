682ffc1c0ed8cf479c54a9c28dbbee32d86539e8
ChloeArd <chloe.ardoise@gmail.com>
2022-04-27 17:02:41 +0200

    Google drive

--- src/Controller/API/UserController.php
+++ src/Controller/API/UserController.php
@@ -5,6 +5,8 @@ namespace App\Controller\API;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,13 +37,24 @@ class UserController extends AbstractController
     {
         $payload = json_decode($request->getContent(), true);
         if(!isset($payload['name']) || !isset($payload['email']) || !isset($payload['image']) || !isset($payload['roles']) || !isset($payload['password']) || !isset($payload['id_google'])) {
-            return $this->returnError($payload);
+            return $this->returnError('Il manque des paramètres');
         }
 
         $user = new User();
         $this->entityManager->persist($user);
         $this->entityManager->flush();
         return $this->json($user);
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    #[Route('/api/user/delete/{id}', name: 'api_user_delete', methods: ['DELETE'])]
+    public function deleteUser(User $user): JsonResponse
+    {
+        $this->userRepository->remove($user);
+        return $this->json(['status' => true,]);
     }
 
     private function returnError(string $errorMessage): JsonResponse
